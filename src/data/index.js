99353d30c5ecac2e956e27b63c4b4107cf9e0b5e
jhj774 <jhj774@naver.com>
2022-09-07 00:09:51 +0900

    std1

---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+export default[
+    {
+        writer: "메타넷",
+        title: "정효재",
+        content: "1"
+    },
+    {
+        writer: "메타넷2",
+        title: "정효재2",
+        content: "2"
+    },
+    {
+        writer: "메타넷3",
+        title: "정효재3",
+        content: "3"
+    }
+]
